3d0d4455f63a6911314663d0bebe87ab587554dc
Prateek Takthar <prateektakthar184b@gmail.com>
2023-12-21 15:31:49 +0530

    update

--- app.js
+++ app.js
@@ -30,13 +30,32 @@ app.use(
 app.use('/api/products', productRoute)
 app.use('/api/category', categoryRoute)
 
-mongoose.connect(MONGO_URL)
+// const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/your_database_name';
+// const PORT = process.env.PORT || 3040;
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("Database connected succesfully :)");
+        console.log("Database connected successfully :)");
         app.listen(PORT, (err) => {
-            if (err) console.log(err.message)
-            console.log('app is running on port', PORT)
-        })
-    }).catch((error) => {
-        console.log("error while connecting to the database ", error);
+            if (err) {
+                console.error('Error starting the server:', err.message);
+            } else {
+                console.log('App is running on port', PORT);
+            }
+        });
     })
+    .catch((error) => {
+        console.error("Error while connecting to the database:", error.message);
+    });
+
+
+// mongoose.connect(MONGO_URL)
+//     .then(() => {
+//         console.log("Database connected succesfully :)");
+//         app.listen(PORT, (err) => {
+//             if (err) console.log(err.message)
+//             console.log('app is running on port', PORT)
+//         })
+//     }).catch((error) => {
+//         console.log("error while connecting to the database ", error);
+//     })
