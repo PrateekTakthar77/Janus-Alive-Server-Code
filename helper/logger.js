e3ec981640dce8e813d759e690978756c94a67dc
Prateek Takthar <prateektakthar184b@gmail.com>
2024-01-30 18:16:46 +0530

    update

--- helper/logger.js
+++ helper/logger.js
@@ -78,7 +78,7 @@ const logger = createLogger({
     ),
     transports: [
         new transports.Console(),
-        new transports.File({ filename: 'server.log' }),
+        // new transports.File({ filename: 'server.log' }),
     ],
 });
 
